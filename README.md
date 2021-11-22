072e2de5457801cb38b819dc2129f74d0c3cb1b1
St4d3ntG4zm4n <bwonder91@live.com>
2021-11-21 18:54:47 -0700

    password generator file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # password-generator-starter-003
+
+<img width="1381" alt="Screen Shot 2021-11-21 at 6 37 49 PM" src="https://user-images.githubusercontent.com/92010483/142789279-075762e7-fe28-4903-a436-9c79385e3e21.png">
+
+## did my best to try and generate a password signing up for more tutor and doing freecodecamp.org to get better at this javascript pushing a screenshot of what happens when you click the button.
+
+[title](https://guzmang2023.github.io/password-generator-starter-003/) <~~ click here
--- script.js
+++ script.js
@@ -1,17 +1,81 @@
 // Assignment code here
 
+  var lowerCase = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","k","r","s","t","u","v","w","x","y","z"]
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+  var specialSymbols = ["!","@","#","$","%","^","&","*","?"]
 
+// global variables always at the top
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  var generateBtn = document.getElementById("generate");
+
+
+
+// functions are always in the middle
 
 // Write password to the #password input
+  function runningForm() {
+    var text;
+    var passwordLength = parseInt(prompt("How long do you want your password to be? (8-128 characters)"));
+    if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password Most Be Greater then 8 And Less then 128?")
+    return ""
+  }
+  var lower =  confirm("Would you like lowercase letters")
+  var upper = confirm("Would you like UPPERCASE Letters?")
+  var special = confirm("Would you like Special Symbols?")
+  var digits = confirm("Would you like Numbers?")
+
+    if (lower === false && upper === false && special === false && digits === false) {
+      alert ("Must choose One of these.")
+  
+      var lower =  confirm("Would you like lowercase letters")
+      var upper = confirm("Would you like UPPERCASE Letters?")
+      var special = confirm("Would you like Special Symbols?")
+      var digits = confirm("Would you like Numbers?")
+
+
+    return ""
+  }
+
+    var randomCode = ""  
+    
+    for (var i = 0; i < confirmLength; i++) {
+      randomCode = randomCode + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      console.log(randomCode)
+    }
+  
+
+  var storageX = {
+    passwordLength: passwordLength,
+    lower: lower,
+    upper: upper,
+    digits: digits,
+    special: special,
+  };
+
+  return storageX
+
+
+  //var passwordString = "this is eventually going to be the password"
+  //return passwordString
+}
+
+function generatePassword() {
+  var allChars = [];
+  var resultPass = "";
+}
+// need to create a generate password function inside this function create empty arrays  var resolte = [] 
+
+
 function writePassword() {
+  // create a variable to get a callback that is the generated password
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("password");
 
   passwordText.value = password;
 
 }
-
+// event listeners and function calls are always at the bottom
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
